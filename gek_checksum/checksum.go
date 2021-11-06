1de546677ce8ef5b6c5e0e765e26dd209196d2d8
gek64 <10346089+gek64@users.noreply.github.com>
2021-11-07 03:15:38 +0800

    Add files via upload

--- gek_checksum/checksum.go
+++ gek_checksum/checksum.go
@@ -5,106 +5,155 @@ import (
 	"crypto/sha1"
 	"crypto/sha256"
 	"encoding/hex"
+	"fmt"
 	"hash/crc32"
 	"io"
-	"log"
 	"os"
 )
 
 // Checksum 校验和计算
-func Checksum(mode string, fileURL string) string {
+func Checksum(mode string, fileURL string) (string, error) {
 	var result string
+	var err error
 	switch mode {
 	case "crc32":
-		result = crc32Sum(fileURL)
+		result, err = crc32Sum(fileURL)
 	case "md5":
-		result = md5Sum(fileURL)
+		result, err = md5Sum(fileURL)
 	case "sha1":
-		result = sha1Sum(fileURL)
+		result, err = sha1Sum(fileURL)
 	case "sha256":
-		result = sha256Sum(fileURL)
+		result, err = sha256Sum(fileURL)
 	default:
-		return ""
+		return "", fmt.Errorf("%s is not a valid mode", mode)
 	}
-	return result
+	return result, err
 }
 
 // crc32Sum crc32校验和
-func crc32Sum(fileURL string) string {
+func crc32Sum(fileURL string) (string, error) {
+	// check fileURL is no link to a folder
+	fileInfo, err := os.Stat(fileURL)
+	if err != nil {
+		return "", err
+	}
+	if fileInfo.IsDir() {
+		return "", fmt.Errorf("%s is a folder,and will be skiped", fileURL)
+	}
+
 	// open file
 	fileObj, err := os.Open(fileURL)
 	if err != nil {
-		log.Fatal(err)
+		return "", err
 	}
+
 	// copy file data
 	hash := crc32.NewIEEE()
 	_, err = io.Copy(hash, fileObj)
+
 	// hex to string
 	hashString := hex.EncodeToString(hash.Sum(nil))
+
 	// close file
 	err = fileObj.Close()
 	if err != nil {
-		log.Fatal(err)
+		return "", err
 	}
-	return hashString
+	return hashString, nil
 }
 
 // md5Sum md5校验和
-func md5Sum(fileURL string) string {
+func md5Sum(fileURL string) (string, error) {
+	// check fileURL is no link to a folder
+	fileInfo, err := os.Stat(fileURL)
+	if err != nil {
+		return "", err
+	}
+	if fileInfo.IsDir() {
+		return "", fmt.Errorf("%s is a folder,and will be skiped", fileURL)
+	}
+
 	// open file
 	fileObj, err := os.Open(fileURL)
 	if err != nil {
-		log.Fatal(err)
+		return "", err
 	}
+
 	// copy file data
 	hash := md5.New()
 	_, err = io.Copy(hash, fileObj)
+
 	// close file
 	err = fileObj.Close()
 	if err != nil {
-		log.Fatal(err)
+		return "", err
 	}
+
 	// hex to string
 	hashString := hex.EncodeToString(hash.Sum(nil))
-	return hashString
+	return hashString, nil
 }
 
 // sha1Sum sha1校验和
-func sha1Sum(fileURL string) string {
+func sha1Sum(fileURL string) (string, error) {
+	// check fileURL is no link to a folder
+	fileInfo, err := os.Stat(fileURL)
+	if err != nil {
+		return "", err
+	}
+	if fileInfo.IsDir() {
+		return "", fmt.Errorf("%s is a folder,and will be skiped", fileURL)
+	}
+
 	// open file
 	fileObj, err := os.Open(fileURL)
 	if err != nil {
-		log.Fatal(err)
+		return "", err
 	}
+
 	// copy file data
 	hash := sha1.New()
 	_, err = io.Copy(hash, fileObj)
+
 	// close file
 	err = fileObj.Close()
 	if err != nil {
-		log.Fatal(err)
+		return "", err
 	}
+
 	// hex to string
 	hashString := hex.EncodeToString(hash.Sum(nil))
-	return hashString
+	return hashString, nil
 }
 
 // sha256Sum sha256校验和
-func sha256Sum(fileURL string) string {
+func sha256Sum(fileURL string) (string, error) {
+	// check fileURL is no link to a folder
+	fileInfo, err := os.Stat(fileURL)
+	if err != nil {
+		return "", err
+	}
+	if fileInfo.IsDir() {
+		return "", fmt.Errorf("%s is a folder,and will be skiped", fileURL)
+	}
+
 	// open file
 	fileObj, err := os.Open(fileURL)
 	if err != nil {
-		log.Fatal(err)
+		return "", err
 	}
+
 	// copy file data
 	hash := sha256.New()
 	_, err = io.Copy(hash, fileObj)
+
 	// close file
 	err = fileObj.Close()
 	if err != nil {
-		log.Fatal(err)
+		return "", err
 	}
+
 	// hex to string
 	hashString := hex.EncodeToString(hash.Sum(nil))
-	return hashString
+	return hashString, nil
 }
