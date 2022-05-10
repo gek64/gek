bf23eb4178a876fd423467f850439c7d346428cd
gek64 <10346089+gek64@users.noreply.github.com>
2022-05-10 19:26:36 +0800

    update gek_app resources

--- gek_app/resources.go
+++ gek_app/resources.go
@@ -3,8 +3,10 @@ package gek_app
 import (
 	"fmt"
 	"gek_downloader"
+	"io/ioutil"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 type Resources struct {
@@ -23,8 +25,8 @@ func NewResources(files []string, urls []string, location string, uninstallDelet
 	return Resources{Files: files, Urls: urls, Location: location, UninstallDeleteLocationFolder: uninstallDeleteLocationFolder}
 }
 
-// Install 安装资源
-func (r Resources) Install() (err error) {
+// InstallFromInternet 从网络安装资源
+func (r Resources) InstallFromInternet() (err error) {
 	// 检测资源安装路径是否存在
 	// 不存在则创建
 	_, err = os.Stat(r.Location)
@@ -45,6 +47,53 @@ func (r Resources) Install() (err error) {
 	// 赋权644
 	for _, file := range r.Files {
 		err = os.Chmod(filepath.Join(r.Location, file), 0644)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// InstallFromLocation 从本地目录安装资源
+func (r Resources) InstallFromLocation(localLocation string) (err error) {
+	// 检查本地路径是否存在
+	_, err = os.Stat(localLocation)
+	if os.IsNotExist(err) {
+		return fmt.Errorf("%s is not exist", localLocation)
+	}
+
+	// 检测资源安装路径是否存在
+	// 不存在则创建
+	_, err = os.Stat(r.Location)
+	if os.IsNotExist(err) {
+		err = os.MkdirAll(r.Location, 0755)
+		if err != nil {
+			return err
+		}
+	}
+
+	// 拷贝文件夹中资源文件到安装文件夹
+	fileInfos, err := ioutil.ReadDir(localLocation)
+	if err != nil {
+		return err
+	}
+
+	for _, rFile := range r.Files {
+		for _, file := range fileInfos {
+			if strings.Contains(file.Name(), rFile) {
+				err = CopyFiles(filepath.Join(localLocation, file.Name()), filepath.Join(r.Location, rFile))
+				if err != nil {
+					return err
+				}
+				break
+			}
+		}
+	}
+
+	// 赋权0644
+	for _, rFile := range r.Files {
+		err = os.Chmod(filepath.Join(r.Location, rFile), 0644)
 		if err != nil {
 			return err
 		}
--- gek_net/ip.go
+++ gek_net/ip.go
@@ -4,13 +4,8 @@ import (
 	"net/netip"
 )
 
-// IsLoopback IP是否为回环地址
-func IsLoopback(ip string) (isLoopback bool, err error) {
-	var blocks = []string{
-		// Loopback
-		"127.0.0.0/8",
-		"::1/128",
-	}
+// IsInBlocks IP是否包含在地址快中
+func IsInBlocks(ip string, blocks []string) (isInBlocks bool, err error) {
 	// 解析ip
 	addr, err := netip.ParseAddr(ip)
 	if err != nil {
@@ -28,6 +23,16 @@ func IsLoopback(ip string) (isLoopback bool, err error) {
 	}
 
 	return false, nil
+}
+
+// IsLoopback IP是否为回环地址
+func IsLoopback(ip string) (isLoopback bool, err error) {
+	var blocks = []string{
+		// Loopback
+		"127.0.0.0/8",
+		"::1/128",
+	}
+	return IsInBlocks(ip, blocks)
 }
 
 // IsPrivate IP是否为专用地址
@@ -39,23 +44,7 @@ func IsPrivate(ip string) (isPrivate bool, err error) {
 		"192.168.0.0/16",
 		"fc00::/7",
 	}
-	// 解析ip
-	addr, err := netip.ParseAddr(ip)
-	if err != nil {
-		return false, err
-	}
-	// ip是否包含在块中
-	for _, blk := range blocks {
-		prefix, err := netip.ParsePrefix(blk)
-		if err != nil {
-			return false, err
-		}
-		if prefix.Contains(addr) {
-			return true, nil
-		}
-	}
-
-	return false, nil
+	return IsInBlocks(ip, blocks)
 }
 
 // IsLinkLocal IP是否为链路本地地址
@@ -65,23 +54,7 @@ func IsLinkLocal(ip string) (isLinkLocal bool, err error) {
 		"169.254.0.0/16",
 		"fe80::/10",
 	}
-	// 解析ip
-	addr, err := netip.ParseAddr(ip)
-	if err != nil {
-		return false, err
-	}
-	// ip是否包含在块中
-	for _, blk := range blocks {
-		prefix, err := netip.ParsePrefix(blk)
-		if err != nil {
-			return false, err
-		}
-		if prefix.Contains(addr) {
-			return true, nil
-		}
-	}
-
-	return false, nil
+	return IsInBlocks(ip, blocks)
 }
 
 // IsPublic IP是否为公网地址
