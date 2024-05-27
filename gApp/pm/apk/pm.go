c1db1d4ea68fb28b9b58dd5bbe9bf50c5d76d3a7
gek64 <10346089+gek64@users.noreply.github.com>
2024-05-27 15:45:24 +0800

    add alpine apk support

---
+++ gApp/pm/apk/pm.go
@@ -0,0 +1,25 @@
+package apk
+
+type Pm struct {
+	Name string
+}
+
+func NewPm(name string) (p *Pm) {
+	return &Pm{Name: name}
+}
+
+func (p *Pm) Install() (err error) {
+	err = Refresh()
+	if err != nil {
+		return err
+	}
+	return Install(p.Name)
+}
+
+func (p *Pm) Uninstall() (err error) {
+	return Uninstall(p.Name)
+}
+
+func (p *Pm) Update() (err error) {
+	return Update(p.Name)
+}
