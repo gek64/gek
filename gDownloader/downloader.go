81e54aefcdc69cc565a67f54fdf954ce86de4af3
gek64 <10346089+gek64@users.noreply.github.com>
2024-06-27 16:08:58 +0800

    update gDownload

--- gDownloader/downloader.go
+++ gDownloader/downloader.go
@@ -1,7 +1,6 @@
 package gDownloader
 
 import (
-	"fmt"
 	"github.com/gek64/gek/gExec"
 	"io"
 	"net/http"
@@ -21,11 +20,11 @@ func Download(fileUrl string, outputFile string, outputFolder string) (err error
 
 	// 输出文件
 	if outputFile == "" {
-		outputFile = path.Base(fileUrl)
+		outputFile = path.Base(resp.Request.URL.Path)
 	}
 
-	// 输出文件夹
-	if outputFolder != "" {
+	// 仅有这一种情况需要进行文件与输出路径的拼接
+	if filepath.Dir(outputFile) == "." && outputFolder != "" {
 		outputFile = filepath.Join(outputFolder, outputFile)
 	}
 
@@ -49,39 +48,26 @@ func Download(fileUrl string, outputFile string, outputFolder string) (err error
 
 // DownloadWithCurl 使用 curl 下载
 func DownloadWithCurl(fileUrl string, outputFile string, outputFolder string) (err error) {
-	var cmd *exec.Cmd
-
-	if outputFolder == "" {
-		if outputFile == "" {
-			cmd = exec.Command("curl", "-LOJ", fileUrl)
-		} else {
-			err = os.MkdirAll(filepath.Dir(outputFile), 0755)
-			if err != nil {
-				return err
-			}
-
-			cmd = exec.Command("curl", "-Lo", outputFile, fileUrl)
+	// 参数中未指定输出文件名时,使用参数中下载url的最后路径作为文件名
+	if outputFile == "" {
+		resp, err := http.Get(fileUrl)
+		if err != nil {
+			return err
 		}
-	} else {
-		if outputFile == "" {
-			err = os.MkdirAll(outputFolder, 0755)
-			if err != nil {
-				return err
-			}
-
-			cmd = exec.Command("curl", "-LOJ", fileUrl)
-			cmd.Dir = outputFolder
-		} else {
-			outputFile = filepath.Join(outputFolder, outputFile)
-			err = os.MkdirAll(filepath.Dir(outputFile), 0755)
-			if err != nil {
-				return err
-			}
-
-			cmd = exec.Command("curl", "-Lo", outputFile, fileUrl)
-			fmt.Println(cmd.String())
-		}
+		outputFile = path.Base(resp.Request.URL.Path)
 	}
 
+	// 仅有这一种情况需要进行文件与输出路径的拼接
+	if filepath.Dir(outputFile) == "." && outputFolder != "" {
+		outputFile = filepath.Join(outputFolder, outputFile)
+	}
+
+	// 创建最终输出文件夹
+	err = os.MkdirAll(filepath.Dir(outputFile), 0755)
+	if err != nil {
+		return err
+	}
+
+	cmd := exec.Command("curl", "-Lo", outputFile, fileUrl)
 	return gExec.Run(cmd)
 }
--- go.mod
+++ go.mod
@@ -3,12 +3,12 @@ module github.com/gek64/gek
 go 1.21.1
 
 require (
-	github.com/aws/aws-sdk-go v1.53.10
+	github.com/aws/aws-sdk-go v1.54.9
 	github.com/tink-crypto/tink-go/v2 v2.2.0
-	golang.org/x/crypto v0.23.0
+	golang.org/x/crypto v0.24.0
 )
 
 require (
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
-	golang.org/x/sys v0.20.0 // indirect
+	golang.org/x/sys v0.21.0 // indirect
 )
